df2ab86e6e5f747f7caec1bd8fb9b22ac81b3a49
ismail-hasan-tanjer <tanjerinfo@gmail.com>
2022-10-20 07:19:56 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,111 @@
+<?php
+
+require_once "vendor/autoload.php";
+
+use App\classes\HelloWorld;
+
+$arr = [
+    "f_name" => [
+        "f_name_1" => "Md",
+        "ff_name_2" => "Ismail"
+    ],
+        
+    "l_name" => "Tanjer",
+];
+
+
+
+
+
+echo ($arr ['f_name']['f_name_1'].' '.$arr ['f_name']['ff_name_2'].' '.$arr['l_name']);
+
+
+
+// $name = 'tanjer';
+// $name = '45';
+
+// echo $name;
+
+
+
+
+// echo ('$name');
+
+
+// $students = ['tanjer', 'roman', 'mahabir', 'faizah'];
+
+
+
+// $students = ['tanjer', 'tanvir', 'jannat', 'mahabir', 'faizah'];
+
+// $NewData = array_values($students);
+
+// echo "<pre>";
+// print_r($NewData);
+// echo "</pre>";
+
+
+
+
+// $list = array("Tanjer"=>"25", "Sad"=>"37", "Saiful"=>"43");
+
+// foreach($list as $x => $value) {
+//   echo "Key=" . $x . ", Value=" . $value;
+//   echo "<br>";
+// }
+
+
+// $arr = [];
+
+// $arr['realme'] ['model1']= 'gt-5';
+// $arr['realme'] ['price']= '10000';
+
+// $arr['oppo'] ['model1']= 'gt-5';
+// $arr['oppo'] ['price']= '15000';
+
+// 
+
+// $numbers = [1,2,3,4];
+
+// $tanjer = array_reverse($numbers);
+// echo "<pre>";
+// print_r($tanjer);
+// echo "</pre>";
+
+// function functionName()
+// {
+//     return 'hi';
+
+// }
+
+// functionName();
+
+// <!-- <table border="2" cellspacing="2px" cellpadding="6px">
+//     <tr>
+//         <th>
+//             Model
+//         </th>
+//         <th>
+//             Price
+//         </th>
+//     </tr>
+ 
+    // foreach($arr as $arr){
+        // echo '
+        // <tr>
+        //     <td>
+        //         Oppo
+        //     </td>
+        //     // <td>
+        //         12000
+        //     </td>
+        // </tr>
+    //     ';
+
+    // // } -->
+    // 
+    
+    ?> 
+
+
+<!-- </table> -->
